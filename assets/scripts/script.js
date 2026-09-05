1d91a4b5e0726386b1aef6024b04f6478f6983cb
Nathan Cen <nathancen9@hotmail.com>
2026-09-05 12:06:55 -0500

    fix:  added null checks before adding event handlers to buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,48 +1,48 @@
 const subscribeBtn = document.querySelector('.footer-newsletter input[type="submit"]');
 
-subscribeBtn.addEventListener('click', () => {
+subscribeBtn?.addEventListener('click', () => {
     alert('Thank you for subsribing.');
 });
 
 const feedbackSubmitBtn = document.querySelector('.contact-us-content input[type="submit"]');
 
-feedbackSubmitBtn.addEventListener('click', () => {
+feedbackSubmitBtn?.addEventListener('click', () => {
     alert('Thank you for your message.');
 });
 
 const addToCartBtns = document.querySelectorAll('.add-to-cart-btn');
 
-addToCartBtns.forEach(btn => btn.addEventListener('click', () => {
+addToCartBtns?.forEach(btn => btn.addEventListener('click', () => {
     alert('Item added to the cart.');
 }));
 
 const processOrderBtn = document.querySelector('.process-order-btn');
 
-processOrderBtn.addEventListener('click', () => {
+processOrderBtn?.addEventListener('click', () => {
     alert('Thank you for your order.');
 });
 
 const clearCartBtn = document.querySelector('.clear-cart-btn');
 
-clearCartBtn.addEventListener('click', () => {
+clearCartBtn?.addEventListener('click', () => {
     alert('Cart cleared.');
 });
 
 const cartModal = document.querySelector('.cart-modal');
 const openCartBtn = document.querySelector('.open-cart-btn');
 
-openCartBtn.addEventListener('click', () => {
+openCartBtn?.addEventListener('click', () => {
     cartModal.showModal();
 });
 
 const closeCartBtn = document.querySelector('.close-cart-btn');
 
-closeCartBtn.addEventListener('click', () => {
+closeCartBtn?.addEventListener('click', () => {
     cartModal.close();
 });
 
 const shopNowBtn = document.querySelector('.shop-now-btn');
 
-shopNowBtn.addEventListener('click', () => {
+shopNowBtn?.addEventListener('click', () => {
     cartModal.close();
 });
