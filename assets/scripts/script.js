4e16d3c99de1e4306a544089f4a56c8d20c73f17
Nathan Cen <nathancen9@hotmail.com>
2026-09-06 10:34:44 -0500

    feat:  shopping cart functionality implementation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,22 +10,31 @@ feedbackSubmitBtn?.addEventListener('click', () => {
     alert('Thank you for your message.');
 });
 
+var cartItems = JSON.parse(sessionStorage.getItem("cartItems")) || [];
 const addToCartBtns = document.querySelectorAll('.add-to-cart-btn');
 
-addToCartBtns?.forEach(btn => btn.addEventListener('click', () => {
-    alert('Item added to the cart.');
+addToCartBtns?.forEach(btn => btn.addEventListener('click', function(e) {
+    const itemName = this.getAttribute('data-item-name');
+    const imageSource = this.getAttribute('data-image-source');
+
+    addToCart(itemName, imageSource);
+
+    alert('Your item has been added to the cart');
 }));
+
+const clearCartBtn = document.querySelector('.clear-cart-btn');
+
+clearCartBtn?.addEventListener('click', () => {
+    clearCart();
+    refreshCart();
+});
 
 const processOrderBtn = document.querySelector('.process-order-btn');
 
 processOrderBtn?.addEventListener('click', () => {
     alert('Thank you for your order.');
-});
-
-const clearCartBtn = document.querySelector('.clear-cart-btn');
-
-clearCartBtn?.addEventListener('click', () => {
-    alert('Cart cleared.');
+    clearCart();
+    refreshCart();
 });
 
 const cartModal = document.querySelector('.cart-modal');
@@ -33,6 +42,7 @@ const openCartBtn = document.querySelector('.open-cart-btn');
 
 openCartBtn?.addEventListener('click', () => {
     cartModal.showModal();
+    refreshCart();
 });
 
 const closeCartBtn = document.querySelector('.close-cart-btn');
@@ -41,8 +51,46 @@ closeCartBtn?.addEventListener('click', () => {
     cartModal.close();
 });
 
-const shopNowBtn = document.querySelector('.shop-now-btn');
+function addToCart(itemName, imageSource) {
+    cartItems.push({ itemName, imageSource });
+    sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
 
-shopNowBtn?.addEventListener('click', () => {
-    cartModal.close();
-});
+function clearCart() {
+    cartItems = [];
+    sessionStorage.removeItem('cartItems');
+}
+
+function refreshCart() {
+    const cart = document.querySelector('.cart-items');
+
+    cart.innerHTML = '';
+
+    if (!cartItems || cartItems.length === 0) 
+    {
+        cart.innerHTML = `
+            <div class="empty-cart">
+                <h2>Your Cart is Empty</h2>
+                <p>Dive into the weekly favorites</p>
+                <button class="shop-now-btn">Shop Now</button>
+            </div>
+        `;
+
+        const shopNowBtn = cart.querySelector('.shop-now-btn');
+
+        shopNowBtn?.addEventListener('click', () => {
+            cartModal.close();
+        });
+
+        return;
+    }
+
+    const contents = cartItems.map(item => `
+        <div class="cart-item">
+            <img src="${item.imageSource}">
+            <h2>${item.itemName}</h2>
+        </div>
+    `).join('');
+
+    cart.innerHTML = contents;
+}
